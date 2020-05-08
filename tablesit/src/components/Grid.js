308bc0b36d4ff4c2d34795e5507d5ed8630c3a06
Sam Delmerico <svdelmerico@gmail.com>
2020-05-08 18:33:52 -0400

    started translating Grid to js

---
+++ tablesit/src/components/Grid.js
@@ -0,0 +1,192 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Desk } from './Desk';
+
+/**
+   Grids are laid out so that the origin is the top left corner of the office/map/etc.
+*/
+export class Grid extends React.Component {
+    static propTypes = {
+        height: PropTypes.number.isRequired,
+        length: PropTypes.number.isRequired,
+        desk_x: PropTypes.number,
+        desk_y: PropTypes.number,
+        grid: PropTypes.array
+    }
+    static defaultProps = {
+        desk_x: 1,
+        desk_y: 1,
+    }
+
+    constructor(props) {
+        super(props);
+
+
+        let grid;
+        if (this.props.grid.length > 0) {
+            grid = props.grid;
+        } else {
+            grid = new Array(props.height);
+            for (let i = 0; i < props.height; i++) {
+                grid = new Array(props.length);
+            }
+        }
+
+        this.state = {
+            grid: grid
+        };
+    }
+
+    render() {
+        return <h1> GRIDSDSD </h1>;
+    }
+
+    seating(social_distance) {
+    /**
+        DFS algorithm to create a list of desks that are:
+        1) properly socially distanced and
+        2) valid places to sit (according to our office layout)
+    */
+
+        let searched_desks = [];
+        let good_desks = [];
+        let unsearched_desks = [{x:0,y:0}];
+
+        while (unsearched_desks.length > 0) {
+            let {dx, dy} = unsearched_desks.pop();
+            searched_desks.append((dx, dy));
+
+            let next_desks = [];
+            for (let i = 0; i < 2; i++) {
+                for (let j = 0; j < 2; j++) {
+                    next_desks.append({
+                        x: Math.min(dx + 1, this.props.height - 1),
+                        y: Math.min(dy + 1, this.props.length - 1)
+                    });
+                }
+            }
+
+            for (let d in next_desks) {
+                if (searched_desks.includes(d)) {
+                    continue;
+                } else {
+                    unsearched_desks.append(d);
+                }
+
+                let desk_obj = this.state.grid[d[0]][d[1]];
+                if (!desk_obj || desk_obj.is_obstruction) {
+                    continue;
+                }
+
+                if (this.desk_is_properly_spaced(d, good_desks, social_distance, {x: this.props.desk_x, y: this.props.desk_y})) {
+                    good_desks.append(d);
+                }
+            }
+
+        }
+
+        return good_desks;
+    }
+
+    desk_is_properly_spaced(d, good_desks, social_distance, {desk_x, desk_y}) {
+        return false;
+    }
+}
+/*
+class Grid:
+    def __init__(self,
+        if height < 1 or length < 1:
+            raise ValueError("height or length cannot be 0")
+
+        self.height = height
+        self.length = length
+        self.desk_dim = (desk_x, desk_y)
+        if grid:
+            self.grid = grid
+        else:
+            self.grid = [[None for i in range(length)] for j in range(height)]
+
+    def set_desk(self, x: int, y: int, d: Desk) -> Desk:
+        """
+        Put a desk object at position (x,y) in our grid.
+        """
+        ret = self.grid[x][y]
+        self.grid[x][y] = d
+
+        return ret
+
+    def get_desk(self, x: int, y: int) -> Desk:
+        return self.grid[x][y]
+
+    def seating(self, social_distance: float) -> [(int, int)]:
+        """
+        """
+        searched_desks = []
+        good_desks = []
+        unsearched_desks = [(0,0)]
+
+        while len(unsearched_desks) > 0:
+            (dx, dy) = unsearched_desks.pop()
+            searched_desks.append((dx, dy))
+
+            next_desks = [
+                (min(dx + i, self.height - 1), min(dy + j, self.length - 1))
+                for i in range(2) for j in range(2)
+            ]
+
+            for d in next_desks:
+                if d in searched_desks:
+                    continue
+                else:
+                    unsearched_desks.append(d)
+
+                desk_obj = self.grid[d[0]][d[1]]
+                if not desk_obj or desk_obj.is_obstruction:
+                    continue
+
+                if desk_is_properly_spaced(d, good_desks, social_distance, self.desk_dim):
+                    good_desks.append(d)
+
+        return good_desks
+
+    def __repr__(self) -> str:
+        s = ""
+        for row in self.grid:
+            s += " ".join(["_" if not d
+                          else "|" if d.is_obstruction
+                          else "*" # if d is a desk
+                          for d in row])
+            s += "\n"
+
+        return s
+
+def desk_is_properly_spaced(d: (int,int), others: [(int,int)], social_distance: float, desk_dim: (float, float)) -> bool:
+    """
+    Check that a particular desk is a certain distance from others in a list.
+    """
+    for o in others:
+        dz = math.sqrt(((d[0]-o[0])/desk_dim[0])**2 + ((d[1]-o[1])/desk_dim[1])**2)
+        if dz <= social_distance:
+            return False
+    return True
+
+if __name__ == "__main__":
+    g = Grid(10, 10)
+    for i in range(20):
+        g.set_desk(random.randint(0,9), random.randint(0,9), Desk(is_obstruction = int(random.randint(0,1))))
+    print(g)
+
+    seats = g.seating(1)
+    for i in range(10):
+        for j in range(10):
+            d = g.get_desk(i,j)
+            if not d or d.is_obstruction:
+                continue
+
+            if (i,j) not in seats:
+                g.set_desk(i,j, None)
+
+    print()
+    print(g)
+
+    */
