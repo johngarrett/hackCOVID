488acf09952c8663a76b9c91cb14675317137247
John Garrett <jacgarrett18@gmail.com>
2020-05-08 22:08:14 -0400

    resolve table rendering

--- tablesit/src/App.js
+++ tablesit/src/App.js
@@ -4,10 +4,56 @@ import './App.css';
 import {Map} from './components/Map.js'
 
 class App extends React.Component {
+     constructor(props) {
+        super(props);
+        this.state = {
+            width: 5,
+            height: 5,
+            tWidth: 1,
+            tHeight: 1,
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({ [name]: value });
+        this.forceUpdate()
+    }
     render() {
         return (
             <div>
-            <Map width="5.0" height="5.0" tWidth = "1.0" tHeight = "1.0"/ >
+            <Map width={this.state.width} height={this.state.height} tWidth={this.state.tWidth} tHeight ={this.state.tHeight}/ >
+            <form>
+                <label>
+                  Map Height: 
+                  <input name="height" type="number"
+                    value={this.state.height}
+                    onChange={this.handleInputChange} />
+                </label>
+                <br></br>
+                <label>
+                  Map Width: 
+                  <input name="width" type="number"
+                    value={this.state.width}
+                    onChange={this.handleInputChange} />
+                </label>
+                <br></br>
+                <label>
+                Table width: 
+                    <input name="tWidth" type="number"
+                      value={this.state.tWidth}
+                      onChange={this.handleInputChange} />
+              </label> 
+                <br></br>
+                <label>
+                  Table Height: 
+                  <input name="tHeight" type="number"
+                    value={this.state.tHeight}
+                    onChange={this.handleInputChange} />
+                </label>
+            </form>
             </div>
         );
     }
--- tablesit/src/components/Map.js
+++ tablesit/src/components/Map.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import BSTable from 'react-bootstrap/Table';
-
+import {Rectangle, Circle, Ellipse, Line, Polyline, CornerBox, Triangle} from 'react-shapes';
 
 export class Map extends React.Component {
     render() {
-        console.log("test")
         return (
             <div>
-            <BSTable>
+            <BSTable class="table table-dark">
                 <tbody>
+                    {this.renderDesks()}
                 </tbody>
             </BSTable>
             <h1>This is a table with a width of {this.props.width} and a height of {this.props.height} </h1>
-            <Table width={this.props.tWidth} height={this.props.tHeight} / >
             </div>
         );
     }
 
-    renderTables() {
+    renderDesks() {
+        let desks = []
+        for (let r = 0; r < this.props.height / this.props.tHeight; r++) {
+            let rowElems = [];
+            for (let c = 0; c < this.props.width / this.props.tWidth; c++) {
+                rowElems.push(<td><Desk width={this.props.tWidth} height={this.props.tHeight} /></td>)
+            }
+            desks.push(<tr>{rowElems}</tr>)
+        }
+        return desks;
     }
 }
 
-class Table extends React.Component {
+class Desk extends React.Component {
     render() {
-        return <p> This is a table with a width of {this.props.width} and height of {this.props.height} </p>;
+        console.log(this.props)
+        return (
+            <Rectangle height={this.props.height * 100} width={this.props.width * 100}/>
+        );
     }
 }
-
